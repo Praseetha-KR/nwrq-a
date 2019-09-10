16752274e98e780d52d4210866488cfb9972ee04
Praseetha-KR <praseetha04@gmail.com>
2019-09-10 13:50:34 +0530

    Updated appcenter apk path

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,6 +1,6 @@
 if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
     set -e
     curl -L https://github.com/appknox/appknox-go/releases/download/0.0.2/appknox-`uname -s`-x86_64 > appknox && chmod +x appknox
-    ./appknox upload $(APPCENTER_OUTPUT_DIRECTORY)/app-debug.apk
+    ./appknox upload $APPCENTER_OUTPUT_DIRECTORY/app-debug.apk
     rm -f appknox
 fi
